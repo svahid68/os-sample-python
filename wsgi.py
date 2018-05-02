d3fd87cab6d23d63b59412b9de35adc83559ae6d
rloisell <rloisell@DC065080.idir.BCGOV>
2018-05-02 15:23:57 -0700

    Lisa L DevOps demo

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! - My Name is Ryan Loiselle, this is the Tuesday Tech Talk about OpenShift and DevOps"
+    return "Hello World! - My Name is Ryan Loiselle, and Lisa is sitting next to me and we are doing the HelloWold demo on OpenShift"
 
 if __name__ == "__main__":
     application.run()
