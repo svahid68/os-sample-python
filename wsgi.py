9cbf2e7b9cb8dac7fc0b255ce86f8b8822a16759
Vahid Sadjadinejad <43013309+svahid68@users.noreply.github.com>
2019-03-28 16:21:08 -0700

    Vhd commit test1

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! - My Name is Ryan Loiselle, and Lisa is sitting next to me and we are doing the HelloWold demo on OpenShift"
+    return "Hello! Vahid here humbly testing openshift."
 
 if __name__ == "__main__":
     application.run()
