db35f5002874ef02884b4ffdc840762ea50e65e9
Vahid Sadjadinejad <43013309+svahid68@users.noreply.github.com>
2019-04-03 16:07:52 -0700

    refined the message to be mine - today!

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello! Vahid here humbly testing openshift."
+    return "Hello! Vahid here humbly testing openshift. 03apr19"
 
 if __name__ == "__main__":
     application.run()
